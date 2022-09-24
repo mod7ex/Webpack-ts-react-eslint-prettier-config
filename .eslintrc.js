d30c9a495dfc6218359561192652e6431f1d25fb
Mourad <mouradface1998@gmail.com>
2022-09-24 21:04:30 +0100

    Prettier & ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+    env: {
+        browser: true,
+        es2021: true,
+    },
 
-  extends: [
-    // "eslint:recommended", // https://github.com/eslint/eslint/issues/8726#issuecomment-308547027
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-  ],
+    extends: [
+        // "eslint:recommended", // https://github.com/eslint/eslint/issues/8726#issuecomment-308547027
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier', // should be the last to override others
+    ],
 
-  overrides: [],
+    overrides: [],
 
-  parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
 
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
 
-  plugins: ["react", "@typescript-eslint"],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
 
-  rules: {
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-  },
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        'prettier/prettier': 'error',
+    },
+
+    ignorePatterns: ['./config/**/*', './public/**/*', './*.config.js'],
 };
--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,15 +1,15 @@
-import { resolve, join } from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import "webpack-dev-server";
+import { resolve } from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import 'webpack-dev-server';
 
-const mode = process.env.NODE_ENV as webpack.Configuration["mode"];
+const mode = process.env.NODE_ENV as webpack.Configuration['mode'];
 
-const IS_MODE = { DEV: mode === "development", PROD: mode === "production" };
+const IS_MODE = { DEV: mode === 'development', PROD: mode === 'production' };
 
 const ROOT_PATH = process.cwd();
 // const ROOT_PATH = path.dirname(__dirname);
@@ -19,10 +19,10 @@ const tsJsTsxJsxRegex = /\.((t|j)sx?)?$/;
 const sassRegex = /\.s[ac]ss$/i;
 
 const getStyleLoaders = () => {
-  const sourceMap = IS_MODE.DEV;
+    const sourceMap = IS_MODE.DEV;
 
-  // prettier-ignore
-  return [
+    // prettier-ignore
+    return [
     IS_MODE.DEV && "style-loader",
     IS_MODE.PROD && MiniCssExtractPlugin.loader,
     { loader: "css-loader", options: { sourceMap } },
@@ -32,92 +32,93 @@ const getStyleLoaders = () => {
 };
 
 type WebpackENV = { WEBPACK_SERVE: boolean };
-type ARGV = Record<string, any> & { env: WebpackENV };
+type ARGV = Record<string, unknown> & { env: WebpackENV };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (env: WebpackENV, argv: ARGV): webpack.Configuration => {
-  const plugins = [
-    new HtmlWebpackPlugin({
-      title: "My App",
-      template: resolve(ROOT_PATH, "public", "index.html"),
-      favicon: resolve(ROOT_PATH, "public", "favicon.ico"),
-    }),
-    IS_MODE.DEV && new BundleAnalyzerPlugin(),
-    IS_MODE.DEV && new ReactRefreshWebpackPlugin(),
-    IS_MODE.DEV && new ForkTsCheckerWebpackPlugin(),
+    const plugins = [
+        new HtmlWebpackPlugin({
+            title: 'My App',
+            template: resolve(ROOT_PATH, 'public', 'index.html'),
+            favicon: resolve(ROOT_PATH, 'public', 'favicon.ico'),
+        }),
+        IS_MODE.DEV && new BundleAnalyzerPlugin(),
+        IS_MODE.DEV && new ReactRefreshWebpackPlugin(),
+        IS_MODE.DEV && new ForkTsCheckerWebpackPlugin(),
 
-    IS_MODE.PROD &&
-      new MiniCssExtractPlugin({
-        filename: "[name]-[contenthash]-bundel.css",
-      }),
-  ].filter(Boolean) as webpack.Configuration["plugins"];
+        IS_MODE.PROD &&
+            new MiniCssExtractPlugin({
+                filename: '[name]-[contenthash]-bundel.css',
+            }),
+    ].filter(Boolean) as webpack.Configuration['plugins'];
 
-  return {
-    mode: mode || "development",
+    return {
+        mode: mode || 'development',
 
-    target: ["browserslist"],
+        target: ['browserslist'],
 
-    entry: [resolve(ROOT_PATH, "config", "index.ts")],
+        entry: [resolve(ROOT_PATH, 'config', 'index.ts')],
 
-    devtool: IS_MODE.PROD ? false : IS_MODE.DEV && "source-map",
+        devtool: IS_MODE.PROD ? false : IS_MODE.DEV && 'source-map',
 
-    output: {
-      filename: "[name]-[fullhash]-bundel.js",
-      path: resolve(ROOT_PATH, "dist"),
-    },
-
-    resolve: {
-      extensions: [".ts", ".js", ".tsx", ".jsx"],
-
-      alias: {
-        "@": resolve(ROOT_PATH, "src", "components"),
-        "~": resolve(ROOT_PATH, "src"),
-      },
-    },
-
-    module: {
-      rules: [
-        {
-          test: tsJsTsxJsxRegex,
-          exclude: /node_modules/,
-          use: "babel-loader",
+        output: {
+            filename: '[name]-[fullhash]-bundel.js',
+            path: resolve(ROOT_PATH, 'dist'),
         },
-        {
-          test: sassRegex,
-          exclude: /node_modules/,
-          use: getStyleLoaders(),
+
+        resolve: {
+            extensions: ['.ts', '.js', '.tsx', '.jsx'],
+
+            alias: {
+                '@': resolve(ROOT_PATH, 'src', 'components'),
+                '~': resolve(ROOT_PATH, 'src'),
+            },
         },
-        {
-          test: /\.(?:ico|png|jpe?g|gif)$/i,
-          type: "asset/resource",
+
+        module: {
+            rules: [
+                {
+                    test: tsJsTsxJsxRegex,
+                    exclude: /node_modules/,
+                    use: 'babel-loader',
+                },
+                {
+                    test: sassRegex,
+                    exclude: /node_modules/,
+                    use: getStyleLoaders(),
+                },
+                {
+                    test: /\.(?:ico|png|jpe?g|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
+                    type: 'asset/inline',
+                },
+            ],
         },
-        {
-          test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
-          type: "asset/inline",
+
+        devServer: {
+            static: {
+                directory: resolve(ROOT_PATH, 'public'),
+            },
+
+            port: 8080,
+
+            hot: true,
+
+            open: true,
+
+            client: {
+                progress: true,
+
+                overlay: {
+                    errors: true,
+                    warnings: false,
+                },
+            },
         },
-      ],
-    },
 
-    devServer: {
-      static: {
-        directory: resolve(ROOT_PATH, "public"),
-      },
-
-      port: 8080,
-
-      hot: true,
-
-      open: true,
-
-      client: {
-        progress: true,
-
-        overlay: {
-          errors: true,
-          warnings: false,
-        },
-      },
-    },
-
-    plugins,
-  };
+        plugins,
+    };
 };
